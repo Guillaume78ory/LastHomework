b61f494d9b13523bb54db3a819412812087989c4
Guillaume78ory <guillaume78.ory@gmail.com>
2024-03-13 01:33:40 +0200

    end

--- Assets/FireBulletOnActivate.cs
+++ Assets/FireBulletOnActivate.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,5 +33,47 @@ public class FireBulletOnActivate : MonoBehaviour
         spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
         Destroy(spawnedBullet, 5);
 
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 100;
+    public AudioClip gunShotSound; // Ajout pour le son du tir
+
+    private AudioSource audioSource; // Source audio pour jouer le son
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+
+        audioSource = gameObject.AddComponent<AudioSource>(); // Ajout du composant AudioSource
+        audioSource.clip = gunShotSound; // Attribution du clip audio
+        audioSource.playOnAwake = false; // Désactivation de la lecture automatique au réveil
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+
+        audioSource.Play(); // Joue le son du tir lorsque la balle est tirée
     }
 }
--- Assets/Teleportationshoot.cs
+++ Assets/Teleportationshoot.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,5 +23,57 @@ public class TargetTeleporter : MonoBehaviour
         }
     }
 }
+*/
 
 
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTeleporter : MonoBehaviour
+{
+    public List<Transform> teleportLocations; // Points de téléportation
+    public GameObject congratulationsPanel; // Panneau de félicitations
+    public AudioClip congratulationsSound; // Son de félicitations
+    public int hitCount = 0; // Nombre de fois que la cible a été touchée
+    private Vector3 originalPosition; // Position originale de la cible
+    private AudioSource audioSource; // Source audio pour jouer le son
+
+    // Initialisation
+    void Start()
+    {
+        // Enregistrez la position originale de la cible
+        originalPosition = transform.position;
+
+        // Configurez l'audio source
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = congratulationsSound;
+        audioSource.playOnAwake = false;
+        congratulationsPanel.SetActive(false); // Assurez-vous que le panneau est désactivé au départ
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Vérifie si l'objet entrant en collision a le tag "Bullet"
+        if (collision.gameObject.tag == "Bullet")
+        {
+            hitCount++; // Incrémente le compteur de touches
+
+            if (hitCount >= 30)
+            {
+                // Faites disparaître la cible et activez le panneau de félicitations
+                gameObject.SetActive(false);
+                congratulationsPanel.transform.position = originalPosition; // Positionnez le panneau là où la cible a été initialement placée
+                congratulationsPanel.SetActive(true);
+                audioSource.Play(); // Jouez le son de félicitations
+            }
+            else if (teleportLocations.Count > 0)
+            {
+                // Téléportez la cible si elle n'a pas encore été touchée 30 fois
+                int randomIndex = Random.Range(0, teleportLocations.Count);
+                Transform selectedLocation = teleportLocations[randomIndex];
+                transform.position = selectedLocation.position;
+            }
+        }
+    }
+}
